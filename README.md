b23765f2942c859d8959099666e94246dfd1e529
y-kurami <yosuke.kurami@gmail.com>
2017-05-15 17:49:42 +0900

    modify

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # ngx-typed-forms
 
+Provides wrapped [Angular's FormBuilder](https://angular.io/docs/ts/latest/api/forms/index/FormBuilder-class.html) to write `AbstractControl<T>`.
+
+It's a workaround for [issue#13721](https://github.com/angular/angular/issues/13721).
+
 ## Install
 
 ```sh
@@ -14,17 +18,50 @@ npm install ngx-typed-forms
 
 ## Usage
 
-```
+You can use this as the original FormBuilder:
+
+```ts
+// import { FormBuilder } from '@angular/forms';
 import { FormBuilder } from 'ngx-typed-forms';
 
-const group = FormBuilder.group({
-  firstName: 'Yosuke',
-  lastName: 'Kurami',
+@Component({...})
+export MyFormComponent {
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    const group = this.formBuilder.group({
+      firstName: 'Yosuke',
+      lastName: 'Kurami',
+    });
+
+    group.valueChanges().subscrive(user => {
+      /* the user argument has a type, { firstName: string; lastName: string } */
+    });
+  }
+}
+```
+
+Or more complex example:
+
+```ts
+interface ComplexForm {
+  name: {
+    first: string;
+    last: string;
+  };
+  age: number;
+  favoriteDishes: string[];
+};
+
+const form = formBuilder.group<ComplexForm>({
+  name: fb.group({ first: 'Yosuke', last: 'Kurami' }),
+  age: 32,
+  favoriteDishes: fb.array<string>([fb.control('faboriteDish')]),
 });
 
-group.valueChanges().subscrive(user => {
-  /* the user argument has a type, { firstName: string; lastName: string } */
-});
+const nameCtrl = form.get('name'); // returns AbstractControl<{ first: string; last: string; }>
+nameCtrl.valueChanges.subscribe(({ last, first }) => console.log(last, first));
 ```
 
 #License
--- src/model.ts
+++ src/model.ts
@@ -10,6 +10,9 @@ export interface AbstractControl<T> extends NgAbstractControl {
   readonly valueChanges: Observable<Partial<T>>;
   readonly value: T;
   setValue(value: Partial<T>, options?: Object): void;
+  get<K extends keyof T>(path: K): AbstractControl<T[K]>;
+  get<S>(path: (string | number)[]): AbstractControl<S>;
+  get(path: (string | number)[]): AbstractControl<any>;
 }
 
 export interface FormControl<T> extends NgFormControl {
@@ -17,18 +20,29 @@ export interface FormControl<T> extends NgFormControl {
   readonly value: T;
   setValue(value: T, options?: Object): void;
   get<K extends keyof T>(path: K): AbstractControl<T[K]>;
+  get<S>(path: (string | number)[]): AbstractControl<S>;
+  get(path: (string | number)[]): AbstractControl<any>;
 }
 
 export interface FormGroup<T> extends NgFormGroup {
-  // readonly controls:{ [P in K]: AbstractControl<T[P]> };
+  // readonly controls:{ [P in keyof T]: AbstractControl<T[P]> }; // if comment out this line, TS2430 occurs.
   readonly valueChanges: Observable<T>;
-  setValue(value: T, options?: Object): void;
   get<K extends keyof T>(path: K): AbstractControl<T[K]>;
+  get<S>(path: (string | number)[]): AbstractControl<S>;
+  get(path: (string | number)[]): AbstractControl<any>;
+  registerControl<K extends keyof T>(name: K, control: AbstractControl<T[K]>): AbstractControl<T[K]>;
+  addControl<K extends keyof T>(name: K, control: AbstractControl<T[K]>): void;
+  setValue(value: T, options?: Object): void;
+  patchValue(value: T, options?: Object): void;
+  getRawValue(): T;
 }
 
 export interface FormArray<T> extends NgFormArray {
   readonly valueChanges: Observable<T[]>;
   readonly value: T[];
+  push(control: AbstractControl<T>): void;
+  insert(index: number, control: AbstractControl<T>): void;
   setValue(value: T[], options?: Object): void;
+  patchValue(value: T[], options?: Object): void;
   getRawValue(): T[];
 }

93b0f27ece0d70b6ac6197f95722c3b76b3357e0
Marcell Calero <marcell_calero@hotmail.com>
2018-09-24 12:11:09 -0300

    Update model.ts

--- src/model.ts
+++ src/model.ts
@@ -10,7 +10,7 @@ export interface AbstractControl<T = any> extends NgAbstractControl {
   readonly valueChanges: Observable<T>;
   readonly value: T;
   setValue(value: T, options?: Object): void;
-  get<K extends keyof T>(path: K): AbstractControl<T[K]>;
+  get<K extends Extract<keyof T, string>>(path: K): AbstractControl<T[K]>;
   get<S>(path: (string | number)[]): AbstractControl<S>;
   get(path: (string | number)[]): AbstractControl<any>;
 }
@@ -19,7 +19,7 @@ export interface FormControl<T = any> extends NgFormControl {
   readonly valueChanges: Observable<T>;
   readonly value: T;
   setValue(value: T, options?: Object): void;
-  get<K extends keyof T>(path: K): AbstractControl<T[K]>;
+  get<K extends Extract<keyof T, string>>(path: K): AbstractControl<T[K]>;
   get<S>(path: (string | number)[]): AbstractControl<S>;
   get(path: (string | number)[]): AbstractControl<any>;
 }
@@ -28,13 +28,13 @@ export interface FormGroup<T = any> extends NgFormGroup {
   // readonly controls:{ [P in keyof T]: AbstractControl<T[P]> }; // if remove this comment, a compilation error(TS2430) occurs.
   readonly valueChanges: Observable<T>;
   readonly value: T;
-  get<K extends keyof T>(path: K): AbstractControl<T[K]>;
+  get<K extends Extract<keyof T, string>>(path: K): AbstractControl<T[K]>;
   get<S>(path: (string | number)[]): AbstractControl<S>;
   get(path: (string | number)[]): AbstractControl<any>;
-  registerControl<K extends keyof T>(name: K, control: AbstractControl<T[K]>): AbstractControl<T[K]>;
-  addControl<K extends keyof T>(name: K, control: AbstractControl<T[K]>): void;
-  setControl<K extends keyof T>(name: K, control: AbstractControl<T[K]>): void;
-  removeControl<K extends keyof T>(name: K): void;
+  registerControl<K extends Extract<keyof T, string>>(name: K, control: AbstractControl<T[K]>): AbstractControl<T[K]>;
+  addControl<K extends Extract<keyof T, string>>(name: K, control: AbstractControl<T[K]>): void;
+  setControl<K extends Extract<keyof T, string>>(name: K, control: AbstractControl<T[K]>): void;
+  removeControl<K extends Extract<keyof T, string>>(name: K): void;
   setValue(value: T, options?: Object): void;
   patchValue(value: T, options?: Object): void;
   getRawValue(): T;
